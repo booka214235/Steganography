b39dd7544a783f2c730a5753ff429698c7eabb86
booka214235 <Mh2401263@tkh.edu.eg>
2025-12-13 23:41:05 +0200

    Update steganography_final.py
    second edge case and changing characters

--- steganography_final.py
+++ steganography_final.py
@@ -1,6 +1,15 @@
-def steganography(secret_message, input_image, output_image):
+def steganography_encoding(secret_message, input_image, output_image):
    
     if secret_message == "":                                                      #first edge case
         return("this is an empty message write any thing to encode")
+    
+    binary_message = ""
+    for char in secret_message:
+        if ord(char) > 127 or ord(char) < 0:                                      #second edge case joo
+            return("the message contains undefined charachters")
+        else:
+            z = format(ord(char), "08b") 
+            binary_message +=z
+    binary_message += "00000000"                                             #delimiter 
     
     
