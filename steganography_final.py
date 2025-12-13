8e8ddd39f642cd7c5905b0b5c24a05d7bc6cdc09
booka214235 <Mh2401263@tkh.edu.eg>
2025-12-13 19:58:36 +0200

    Create steganography_final.py
    first edge case to check if the secret message was empty and to see if git hub is working

---
+++ steganography_final.py
@@ -0,0 +1,6 @@
+def steganography(secret_message, input_image, output_image):
+   
+    if secret_message == "":                                                      #first edge case
+        return("this is an empty message write any thing to encode")
+    
+    
