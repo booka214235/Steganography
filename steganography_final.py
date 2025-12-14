541171b57803ce3da5346d333ea6215c95240705
booka214235 <Mh2401263@tkh.edu.eg>
2025-12-14 03:19:06 +0200

    Update steganography_final.py
    dual input method
    fourth edge case

--- steganography_final.py
+++ steganography_final.py
@@ -1,9 +1,17 @@
-def steganography_encoding(secret_message, input_image, output_image):
-   
+def steganography_encoding(input_image, output_image):
 
     password = input("enter password to encode: ")                              #making password for extra security
     if password !="12345":
         return("wrong password try again") 
+    
+    choice = input("if you want to type the secret message type 1: \nif you want to upload text file type 2:\n")  #dual input that enables uploading file or message
+    if choice == "1":
+        secret_message = input("Enter message: ")
+    
+    elif choice == "2":
+        filename = input("Enter filename: ")
+        with open(filename, 'r') as f:
+            secret_message = f.read()
 
     if secret_message == "":                                                    #first edge case to see if the message was empty
         return("this is an empty message write any thing to encode")
@@ -22,3 +30,6 @@ def steganography_encoding(secret_message, input_image, output_image):
     
     if data[:2] != b'BM':                                                       #third edge case that checks if the image is an bmp extension
         return("This is not a bmp image ") 
+    
+    if len(binary_message) > len(data) - 54:                                    #fourth edge case that checks if the length of message is bigger than image size
+        return("the message is too long that cannot fit the image")
